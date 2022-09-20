016c397b4d2baf7bc2dee268a14687186724bef6
Robkata <r.georgiev2017@pmg-pernik.com>
2022-09-20 14:30:10 +0300

    Last modifies and changes

---
+++ Animals/Animals/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Dog: Animal
+    {
+        public Dog(string name, string favouriteFood)
+        {
+        }
+
+        public override string ExplainMyself()
+        {
+            return base.ExplainMyself() + Environment.NewLine + "DJAAF";
+        }
+    }
+}
